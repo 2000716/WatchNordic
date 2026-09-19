f5263c7afd5e4f38d100f0ddd468381b2b568e15
2000716 <SveinOlavHaukaasArstein@hotmail.com>
2026-09-19 13:42:30 +0200

    Add initIntro function for navigation setup

--- js/intro.js
+++ js/intro.js
@@ -1,1 +1,17 @@
+export function initIntro(router) {
+  const ctaBtn = document.getElementById('intro-cta-btn');
+  const loginLink = document.getElementById('intro-login-link');
 
+  if (ctaBtn) {
+    ctaBtn.addEventListener('click', () => {
+      router.navigateTo('innlogging');
+    });
+  }
+
+  if (loginLink) {
+    loginLink.addEventListener('click', (e) => {
+      e.preventDefault();
+      router.navigateTo('innlogging');
+    });
+  }
+}
